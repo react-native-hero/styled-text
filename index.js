4fffebd7c68153011712673558ac174b49153a64
musicode <musicode@qq.com>
2023-04-19 12:10:19 +0800

    bump 0.0.2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 import React, {
+  createRef,
   PureComponent,
 } from 'react'
 
 import {
   Text,
+  Platform,
   StyleSheet,
 } from 'react-native'
 
@@ -23,9 +25,24 @@ const styles = StyleSheet.create({
   }
 })
 
+// android 的 measure 方法返回全是 undefined
+let measureMethod = 'measureInWindow'
+if (Platform.OS === 'ios') {
+  measureMethod = 'measure'
+}
+
 export class StyledText extends PureComponent {
 
   static defaultProps = {}
+
+  constructor(props) {
+    super(props)
+    this.textRef = createRef()
+  }
+
+  measure(callback) {
+    this.textRef.current[measureMethod](callback)
+  }
 
   render() {
 
@@ -55,83 +72,85 @@ export class StyledText extends PureComponent {
     } = this.props
 
     const renderText = function (text, textProps) {
-      const tokens = parseText(
-        text,
-        patterns
-          ? patterns.map(name => patternMap[name])
-          : undefined
-      )
-      if (tokens.length >= 1) {
-        return (
-          <Text
-            {...textProps}
-          >
-            {
-              tokens.map((token, index) => {
-                let text = token.text, style, onPress
-
-                if (token.type === 'tel') {
-                  style = telStyle || styles.link
-                  if (telText) {
-                    text = telText
-                  }
-                  if (onTelPress) {
-                    onPress = () => {
-                      onTelPress(token.data.tel)
-                    }
-                  }
-                }
-                else if (token.type === 'url') {
-                  style = urlStyle || styles.link
-                  if (urlText) {
-                    text = urlText
-                  }
-                  if (onUrlPress) {
-                    onPress = () => {
-                      onUrlPress(token.data.url)
-                    }
-                  }
-                }
-                else if (token.type === 'email') {
-                  style = emailStyle || styles.link
-                  if (emailText) {
-                    text = emailText
-                  }
-                  if (onEmailPress) {
-                    onPress = () => {
-                      onEmailPress(token.data.email)
-                    }
-                  }
-                }
-                else if (token.type === 'image') {
-                  style = imageStyle || styles.link
-                  if (imageText) {
-                    text = imageText
-                  }
-                  if (onImagePress) {
-                    onPress = () => {
-                      onImagePress(token.data.url)
-                    }
-                  }
-                }
-                else if (token.type === 'highlight') {
-                  style = highlightStyle || styles.highlight
-                  text = token.data.text
-                }
-
-                return (
-                  <Text
-                    key={`${index}-${token.text}`}
-                    style={style}
-                    onPress={onPress}
-                  >
-                    {text}
-                  </Text>
-                )
-              })
-            }
-          </Text>
+      if (patterns == null || patterns.length > 0) {
+        const tokens = parseText(
+          text,
+          patterns
+            ? patterns.map(name => patternMap[name])
+            : undefined
         )
+        if (tokens.length > 0) {
+          return (
+            <Text
+              {...textProps}
+            >
+              {
+                tokens.map((token, index) => {
+                  let text = token.text, style, onPress
+
+                  if (token.type === 'tel') {
+                    style = telStyle || styles.link
+                    if (telText) {
+                      text = telText
+                    }
+                    if (onTelPress) {
+                      onPress = () => {
+                        onTelPress(token.data.tel)
+                      }
+                    }
+                  }
+                  else if (token.type === 'url') {
+                    style = urlStyle || styles.link
+                    if (urlText) {
+                      text = urlText
+                    }
+                    if (onUrlPress) {
+                      onPress = () => {
+                        onUrlPress(token.data.url)
+                      }
+                    }
+                  }
+                  else if (token.type === 'email') {
+                    style = emailStyle || styles.link
+                    if (emailText) {
+                      text = emailText
+                    }
+                    if (onEmailPress) {
+                      onPress = () => {
+                        onEmailPress(token.data.email)
+                      }
+                    }
+                  }
+                  else if (token.type === 'image') {
+                    style = imageStyle || styles.link
+                    if (imageText) {
+                      text = imageText
+                    }
+                    if (onImagePress) {
+                      onPress = () => {
+                        onImagePress(token.data.url)
+                      }
+                    }
+                  }
+                  else if (token.type === 'highlight') {
+                    style = highlightStyle || styles.highlight
+                    text = token.data.text
+                  }
+
+                  return (
+                    <Text
+                      key={`${index}-${token.text}`}
+                      style={style}
+                      onPress={onPress}
+                    >
+                      {text}
+                    </Text>
+                  )
+                })
+              }
+            </Text>
+          )
+        }
       }
       return (
         <Text
@@ -141,6 +160,8 @@ export class StyledText extends PureComponent {
         </Text>
       )
     }
+
+    textProps.ref = this.textRef
 
     if (typeof children === 'string') {
       children = renderText(children, textProps)
